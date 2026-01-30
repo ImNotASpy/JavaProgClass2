7642d0d3856cf2e45c3ad0c4214115ca63e87e48
ImNotASpy <vpwallner@gmail.com>
2026-01-30 05:13:50 -0600

    part 2 done

--- unicode.java
+++ unicode.java
@@ -13,18 +13,42 @@ public class unicode {
     double sqrtValue = Math.sqrt(userDoubleValue); //square root
     System.out.printf("square root is: %.2f%n", sqrtValue);
 
-    double powerValue = Math.pow(sqrtValue, 2); //poweer
-    double roundedValue = Math.round(powerValue); //rounded
+    double powerValue = Math.pow(userDoubleValue, 2); //poweer
+    System.out.printf("power value is: %.2f%n", powerValue);
+
+    double roundedValue = Math.round(userDoubleValue); //rounded
     System.out.printf("rounded value is: %.2f%n", roundedValue);
 
-    double celiValue = Math.ceil(roundedValue); //ceiling
+    double celiValue = Math.ceil(userDoubleValue); //ceiling
     System.out.printf("ceiling value is: %.2f%n", celiValue);
 
-    double floorValue = Math.floor(celiValue); //floor
+    double floorValue = Math.floor(userDoubleValue); //floor
     System.out.printf("floor value is: %.2f%n", floorValue);
 
     //part 2
     System.out.println("Enter a single character (ex: a, 2, !...): ");
     char userChar = user.next().charAt(0); //character input
+
+    if (Character.isUpperCase(userChar)) {
+      System.out.println("The character you entered is an uppercase letter.");
+    } else if (Character.isLowerCase(userChar)) {
+      System.out.println("The character you entered is a lowercase letter.");
+    } else if (Character.isDigit(userChar)) {
+      System.out.println("The character you entered is a digit.");
+    } else {
+      System.out.println("The character you entered is other.");
+    }
+
+    int i = (int) userChar; //unicode transfer
+    System.out.println("The Unicode value of the character is: " + i);
+
+    int x = i + 1; //next unicode character
+    char nextChar = (char) x;
+    System.out.println("The next Unicode character is: " + nextChar);
+
+
+    //part 3
+    System.out.println("Enter a first and last name separated by a space: ");
+    String fullName = user.nextLine(); //full name input
   }
 }
